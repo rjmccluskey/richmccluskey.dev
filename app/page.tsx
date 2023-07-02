4af5a66df9b0ad6bd791544787119661ff812241
Rich McCluskey <hello@richmccluskey.dev>
2023-07-02 14:17:23 -0700

    Add missing li elements under ul for better accessibility

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,14 @@ export default function Home() {
       <nav className="my-16">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm text-zinc-500 duration-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
+            <li key={item.href}>
+              <Link
+                href={item.href}
+                className="text-sm text-zinc-500 duration-500 hover:text-zinc-300"
+              >
+                {item.name}
+              </Link>
+            </li>
           ))}
         </ul>
       </nav>
