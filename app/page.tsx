db483393a9aaccf2142646c522f0e0f505a33448
Rich McCluskey <hello@richmccluskey.dev>
2023-07-02 14:17:24 -0700

    Minor style tweaks on home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,29 @@ const navigation = [
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6 sm:p-12 md:p-24">
+    <main
+      className={`
+        flex
+        min-h-screen
+        flex-col
+        items-center
+        justify-center
+        bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))]
+        from-gray-900
+        to-transparent
+        p-6
+        text-slate-400
+        sm:p-12
+        md:p-24
+      `}
+    >
       <nav className="my-16">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <li key={item.href}>
               <Link
                 href={item.href}
-                className="text-sm text-zinc-500 duration-500 hover:text-zinc-300"
+                className="text-sm duration-500 hover:text-slate-300"
               >
                 {item.name}
               </Link>
@@ -41,13 +56,13 @@ export default function Home() {
         Rich McCluskey
       </h1>
       <div className="my-16 max-w-2xl text-center">
-        <p className="text-sm text-zinc-500 ">
+        <p className="text-sm">
           Hi 👋, my name is Rich. I&apos;m a software engineer from California,
           currently working on the platform team at{' '}
           <Link
             target="_blank"
             href="https://www.dozuki.com"
-            className="underline duration-500 hover:text-zinc-300"
+            className="underline duration-500 hover:text-slate-300"
           >
             Dozuki
           </Link>
