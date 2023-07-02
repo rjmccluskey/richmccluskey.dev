0c04b3e462f84a2c6eb188757abacfb489eb9f61
Rich McCluskey <hello@richmccluskey.dev>
2023-07-02 14:17:22 -0700

    Add basic about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
@@ -19,6 +21,20 @@ export default function Home() {
       >
         Rich McCluskey
       </h1>
+      <div className="my-16 max-w-2xl text-center">
+        <p className="text-sm text-zinc-500 ">
+          Hi 👋, my name is Rich. I&apos;m a software engineer from California,
+          currently working on the platform team at{' '}
+          <Link
+            target="_blank"
+            href="https://www.dozuki.com"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            Dozuki
+          </Link>
+          . In my spare time, I hack on a couple side projects for fun.
+        </p>
+      </div>
     </main>
   );
 }
