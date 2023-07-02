232f475d089206fe9e9353a57ab821d91684cad5
Rich McCluskey <hello@richmccluskey.dev>
2023-07-02 14:17:22 -0700

    Minor code style change: add line breaks

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="bg-gradient-to-r from-sky-500 to-emerald-500 bg-clip-text text-center text-4xl font-extrabold uppercase tracking-widest text-transparent md:text-5xl lg:text-7xl">
+      <h1
+        className={`
+          bg-gradient-to-r
+          from-sky-500
+          to-emerald-500
+          bg-clip-text
+          text-center
+          text-4xl
+          font-extrabold
+          uppercase
+          tracking-widest
+          text-transparent
+          md:text-5xl
+          lg:text-7xl
+        `}
+      >
         Rich McCluskey
       </h1>
     </main>
