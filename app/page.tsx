7b50e1089914fca92a2b5a50d79eb48b6043e36a
Rich McCluskey <hello@richmccluskey.dev>
2023-07-02 14:17:21 -0700

    Center name

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-7xl">Rich McCluskey</h1>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-center text-4xl font-extrabold uppercase tracking-widest md:text-5xl lg:text-7xl">
+        Rich McCluskey
+      </h1>
     </main>
   );
 }
