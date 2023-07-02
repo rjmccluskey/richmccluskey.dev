2318a5d72347fd318215251763015a1f0f8a12d5
Rich McCluskey <hello@richmccluskey.dev>
2023-07-02 14:17:23 -0700

    Add main nav on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
 import Link from 'next/link';
 
+const navigation = [
+  { name: 'Projects', href: '/projects' },
+  { name: 'Contact', href: '/contact' },
+];
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <nav className="my-16">
+        <ul className="flex items-center justify-center gap-4">
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-sm text-zinc-500 duration-500 hover:text-zinc-300"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      </nav>
       <h1
         className={`
           bg-gradient-to-r
